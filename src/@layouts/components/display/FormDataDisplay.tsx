d67d3f26f43001e114ece79a66caa69f3a5794a4
shankarjatin <shankarjatin1005@gmail.com>
2025-02-19 18:33:43 +0530

    styling

--- src/@layouts/components/display/FormDataDisplay.tsx
+++ src/@layouts/components/display/FormDataDisplay.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Typography, Grid, Box, Paper, IconButton } from '@mui/material'
-import { styled } from '@mui/system'
+import { color, styled } from '@mui/system'
 import FileCopyIcon from '@mui/icons-material/FileCopy'
 
 const DataItem = styled(Paper)(({ theme }) => ({
@@ -44,14 +44,14 @@ const FormDataDisplay: React.FC = () => {
         {/* Step 1: Copy the following files */}
         <Grid item xs={12} sm={12} md={12}>
           <DataItem>
-            <Typography variant='h4' gutterBottom className='mb-4'>
+            <Typography variant='h5' gutterBottom className='mb-4'>
               Step 1: Copy the following files
             </Typography>
             <FullDetails variant='body2'>
               CSS File (CDN link type to be pasted in header):
               <div className='flex items-center space-x-2 my-2'>
                 <code>
-                  <pre className='m-0 p-2 rounded-md flex-1'>
+                  <pre className='m-0 p-2 flex-1 bg-[#7367F0] bg-opacity-60 text-white backdrop-blur-md'>
                     {`<link rel="stylesheet" href="https://example.com/your-stylesheet.css">`}
                   </pre>
                 </code>
@@ -65,7 +65,7 @@ const FormDataDisplay: React.FC = () => {
               JS File (Script to be pasted in footer):
               <div className='flex items-center space-x-2 my-2'>
                 <code>
-                  <pre className='m-0 p-2 rounded-md flex-1'>
+                  <pre className='m-0 p-2 flex-1 bg-[#7367F0] bg-opacity-60 text-white backdrop-blur-md'>
                     {`<script src="https://example.com/your-script.js"></script>`}
                   </pre>
                 </code>
@@ -82,13 +82,13 @@ const FormDataDisplay: React.FC = () => {
         {/* Step 2: Paste these files in your header and footer */}
         <Grid item xs={12} sm={12} md={12}>
           <DataItem>
-            <Typography variant='h4' gutterBottom className='mb-4'>
+            <Typography variant='h5' gutterBottom className='mb-4'>
               Step 2: Paste these files in your header and footer
             </Typography>
             <FullDetails variant='body2'>
-              - Paste the CSS file link inside the <code>&lt;head&gt;</code> section.
+              - Paste the CSS file link inside the <code className='m-0 p-2 flex-1 bg-[#7367F0] bg-opacity-60 text-white backdrop-blur-md'>&lt;head&gt;</code> section.
               <br></br>
-              <br />- Paste the JS script inside the <code>&lt;footer&gt;</code> section.
+              <br />- Paste the JS script inside the <code className='m-0 p-2 flex-1 bg-[#7367F0] bg-opacity-60 text-white backdrop-blur-md'>&lt;footer&gt;</code> section.
             </FullDetails>
           </DataItem>
         </Grid>
@@ -96,17 +96,18 @@ const FormDataDisplay: React.FC = () => {
         {/* Step 3: Create the div element */}
         <Grid item xs={12} sm={12} md={12}>
           <DataItem>
-            <Typography variant='h4' gutterBottom>
+            <Typography variant='h5' gutterBottom>
               Step 3: Create the div element with id="OutreachChatbot"
             </Typography>
             <FullDetails variant='body2'>
               Insert the following div tag where you want the chatbot to appear:
               <div className='flex items-center space-x-2 my-2'>
-                <code>
-                  <pre className='m-0 p-2 rounded-md flex-1'>
-                    {`<div id="OutreachChatbot"></div>`}
-                  </pre>
-                </code>
+              <code className='text-[#7367F0]'>
+  <pre className='m-0 p-2 flex-1 bg-[#7367F0] bg-opacity-60 text-white backdrop-blur-md'>
+    {`<div id="OutreachChatbot"></div>`}
+  </pre>
+</code>
+
                 <IconButton
                   onClick={() => handleCopy('<div id="OutreachChatbot"></div>')}
                 >
